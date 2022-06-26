a61d1aeed95fa929864cf89d9f051e2996ea08b2
Bruno Domingues <onurbcd@gmail.com>
2022-06-26 20:20:25 -0300

    add basic api service to use in old api's

--- src/app/@service/me.service.ts
+++ src/app/@service/me.service.ts
@@ -1,11 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AccountPage } from '@model';
-import { ApiService } from '@service/api.service';
+import { BasicApiService } from '@service/basic-api.service';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export class MeService extends ApiService {
+export class MeService extends BasicApiService {
   constructor(protected http: HttpClient) {
     super(http, 'me/');
   }
