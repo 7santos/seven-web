d4e969f0e1c3e36622a319ef51a67cfb0a12f5f0
Bruno Domingues <onurbcd@gmail.com>
2022-06-26 20:37:50 -0300

    refactor api service

--- src/app/@service/api.service.ts
+++ src/app/@service/api.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '@env/environment';
-import { Prime } from '@model';
+import { PageEvent } from '@angular/material/paginator';
+import { Sort } from '@angular/material/sort';
+import { Page, Prime } from '@model';
 
 export interface QueryParams {
   [name: string]: string | string[];
@@ -11,4 +13,59 @@ export abstract class ApiService<E extends Prime, F, ID> {
   constructor(protected httpClient: HttpClient, public baseUrl: string) {}
 
   abstract getQueryParams(filter: F): QueryParams;
+
+  post(entity: E, path: string = ''): Observable<E> {
+    return this.httpClient.post<E>(
+      `${environment.apiUri}${this.baseUrl}${path}`,
+      entity
+    );
+  }
+
+  put(id: ID, entity: E): Observable<E> {
+    return this.httpClient.put<E>(
+      `${environment.apiUri}${this.baseUrl}${id}`,
+      entity,
+      {}
+    );
+  }
+
+  save(id: ID, entity: E): Observable<E> {
+    return id == null ? this.post(entity) : this.put(id, entity);
+  }
+
+  delete(id: ID): Observable<void> {
+    return this.httpClient.delete<void>(
+      `${environment.apiUri}${this.baseUrl}${id}`
+    );
+  }
+
+  get(id: ID): Observable<E> {
+    return this.httpClient.get<E>(`${environment.apiUri}${this.baseUrl}${id}`);
+  }
+
+  getAll(filter: F, pageEvent: PageEvent, sort: Sort): Observable<Page<E>> {
+    const queryParams = this.getQueryParams(filter);
+    const params = this.getHttpParams(queryParams, pageEvent, sort);
+
+    return this.httpClient.get<Page<E>>(
+      `${environment.apiUri}${this.baseUrl}`,
+      {
+        params,
+      }
+    );
+  }
+
+  private getHttpParams(
+    queryParams: QueryParams,
+    pageEvent: PageEvent,
+    sort: Sort
+  ): HttpParams {
+    if (pageEvent) {
+      queryParams['page'] = `${pageEvent.pageIndex}`;
+      queryParams['size'] = `${pageEvent.pageSize}`;
+    }
+
+    queryParams['sort'] = sort.active + ',' + sort.direction;
+    return new HttpParams({ fromObject: queryParams });
+  }
 }
